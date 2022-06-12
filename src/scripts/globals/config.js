ad167995211dbdccb9ba8e4f1e00b34b0d26722f
dedybastiant <dedybastian.t@gmail.com>
2022-06-12 10:28:58 +0700

    add globals file

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+    KEY: '7adb5db23e7e2675b4d598857145f160',
+    BASE_URL: 'https://api.themoviedb.org/3/',
+    BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+    DEFAULT_LANGUAGE: 'en-us',
+  };
+   
+  export default CONFIG;
