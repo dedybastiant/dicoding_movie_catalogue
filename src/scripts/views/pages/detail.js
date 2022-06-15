96714d6d34df3f89018fd810a02e6d596dc4b42f
dedybastiant <dedybastian.t@gmail.com>
2022-06-15 14:22:18 +0700

    add like button element to code and fixing indentation

--- src/scripts/views/pages/detail.js
+++ src/scripts/views/pages/detail.js
@@ -1,17 +1,23 @@
 import TheMovieDbSource from "../../data/themoviedb-source";
 import UrlParser from "../../routes/url-parser";
-import { createMovieDetailTemplate } from "../templates/template-creator";
+import { createLikeButtonTemplate, createMovieDetailTemplate } from "../templates/template-creator";
 
 const Detail = {
     async render() {
-        return `<div id="movies" class="movies">`
+        return `
+            <div id="movies" class="movies"></div>
+            <div id="likeButtonContainer"></div>
+        `
     },
 
     async afterRender() {
         const url = UrlParser.parseActiveUrlWithoutCombiner()
         const movie = await TheMovieDbSource.detailMovie(url.id);
         const moviesContainer = document.querySelector('#movies');
-        moviesContainer.innerHTML = createMovieDetailTemplate(movie)
+        moviesContainer.innerHTML = createMovieDetailTemplate(movie);
+
+        const likeButtonContainer = document.querySelector('#likeButtonContainer');
+        likeButtonContainer.innerHTML = createLikeButtonTemplate();
     }
 }
 
